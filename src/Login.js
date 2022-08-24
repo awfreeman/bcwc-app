94e411874fd243167d9685ac0b643e1cf94a8091
awfreeman <awfreeman1@gmail.com>
2022-08-23 21:30:19 -0400

    initial commit

---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import { Box, Button, FormGroup, TextField } from "@mui/material"
+import { useState } from "react";
+
+
+export default function Login(props) {
+
+    const [uname, setuname] = useState("")
+    const [pass, setpass] = useState("")
+
+    function onLogin(e) {
+        props.LoginCallback({username:uname, token:"Token"})
+    }
+
+    return (
+        <Box
+            component="form">
+            <TextField required id="outlined-required" label="Username" onChange={e => setuname(e.target.value)}/>
+            <TextField required id="outlined-password-input" label="Password" type="password" onChange={e => setpass(e.target.value)}/>
+            <Button variant="contained" onClick={onLogin}>LOGIN</Button>
+        </Box>
+    );
+}
