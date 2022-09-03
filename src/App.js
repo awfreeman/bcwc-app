0b9f45a9357a8a7131853f32f5405d6e5d7e3ce2
Andrew Freeman <awfreeman1@gmail.com>
2022-09-03 19:35:00 -0400

    spacing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Grid2 from '@mui/material/Unstable_Grid2'
-import {Box, Stack, TextField} from '@mui/material'
+import { Box, Stack, TextField } from '@mui/material'
 import { useRecoilState, atom } from 'recoil'
 import SearchApp from './SearchApp.js'
 import Login from './Login.js'
@@ -11,17 +11,17 @@ const userState = atom({ key: 'user', default: null })
 
 
 function App() {
-  const [user, setUser] = useRecoilState(userState)
-  function LoginCallback(user) {
-    setUser(user)
-  }
+    const [user, setUser] = useRecoilState(userState)
+    function LoginCallback(user) {
+        setUser(user)
+    }
 
-  if (user == null) {
-    return (<Login LoginCallback={LoginCallback} />)
-  }
-  else {
-    return (<SearchApp />)
-  }
+    if (user == null) {
+        return (<Login LoginCallback={LoginCallback} />)
+    }
+    else {
+        return (<SearchApp />)
+    }
 }
 
 export default App;
--- src/SearchApp.js
+++ src/SearchApp.js
@@ -1,4 +1,4 @@
-import { Box, ButtonGroup, Stack, TextField, Button, Popper, MenuList, MenuItem, Paper} from '@mui/material'
+import { Box, ButtonGroup, Stack, TextField, Button, Popper, MenuList, MenuItem, Paper } from '@mui/material'
 import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material'
 import { useRecoilState, atom } from 'recoil'
 import AnimalCard from './AnimalCard.js'
@@ -12,69 +12,69 @@ const animal = {
     species: "critter",
     pictureUrl: "character.gif",
     comments: ["comment1", "comment3"]
-  }
+}
 const searchOptions = ["Intake#", "Name", "Species", "Comments"]
 
-const searchTextState = atom({key: 'searchText', default:""})
+const searchTextState = atom({ key: 'searchText', default: "" })
 export default function SearchApp() {
-  const [searchText, setSearchText] = useRecoilState(searchTextState)
-  const [searchOption, setSearchOption] = useState(searchOptions[0])
-  function search(e){
-    e.preventDefault()
-    console.log(searchText)
-  }
-  return (
-    <Stack direction="column" alignItems="center" >
-      <Stack component="form" direction="row" alignContent="stretch" onSubmit={e => search(e)}>
-        <TextField
-          id="outlined-basic"
-          label="Search"
-          variant="outlined"
-          onChange={e => setSearchText(e.target.value)}
-          sx={{ flex: 1 , maxWidth:400}}
-        />
-        <Dropdown callback={setSearchOption} options={searchOptions} selection={searchOption} />
-      </Stack>
-      <div className="animalGrid">
-        <AnimalCard animal={animal} />
-        <AnimalCard animal={animal} />
-        <AnimalCard animal={animal} />
-        <AnimalCard animal={animal} />
-        <AnimalCard animal={animal} />
-      </div>
-    </Stack>
-  )
+    const [searchText, setSearchText] = useRecoilState(searchTextState)
+    const [searchOption, setSearchOption] = useState(searchOptions[0])
+    function search(e) {
+        e.preventDefault()
+        console.log(searchText)
+    }
+    return (
+        <Stack direction="column" alignItems="center" >
+            <Stack component="form" direction="row" alignContent="stretch" onSubmit={e => search(e)}>
+                <TextField
+                    id="outlined-basic"
+                    label="Search"
+                    variant="outlined"
+                    onChange={e => setSearchText(e.target.value)}
+                    sx={{ flex: 1, maxWidth: 400 }}
+                />
+                <Dropdown callback={setSearchOption} options={searchOptions} selection={searchOption} />
+            </Stack>
+            <div className="animalGrid">
+                <AnimalCard animal={animal} />
+                <AnimalCard animal={animal} />
+                <AnimalCard animal={animal} />
+                <AnimalCard animal={animal} />
+                <AnimalCard animal={animal} />
+            </div>
+        </Stack>
+    )
 }
 
-function Dropdown(props){
-  const [popen, setPopen] = useState(false)
-  const anchorRef = useRef(null);
-  let options = []
-  props.options.forEach(elem => {
-    options.push(
-    <MenuItem onClick={() => {
-        props.callback(elem)
+function Dropdown(props) {
+    const [popen, setPopen] = useState(false)
+    const anchorRef = useRef(null);
+    let options = []
+    props.options.forEach(elem => {
+        options.push(
+            <MenuItem onClick={() => {
+                props.callback(elem)
+                setPopen(!popen)
+            }}>{elem}</MenuItem>)
+    })
+
+    function openList(e) {
         setPopen(!popen)
-      }}>{elem}</MenuItem>)
-  })
+    }
 
-  function openList(e){
-    setPopen(!popen)
-  }
-
-  return (
-    <>
-      <ButtonGroup variant="contained" ref={anchorRef}>
-        <Button sx={{textTransform:"none", width:100, fontSize:16}}>{props.selection}</Button>
-        <Button size="small" onClick={e => openList(e)}>{popen ? <ArrowDropUp/> : <ArrowDropDown/>}</Button>
-      </ButtonGroup>
-      <Popper open={popen} anchorEl={anchorRef.current}>
-        <Paper sx={{width:145}}>
-          <MenuList>
-            {options}
-          </MenuList>
-        </Paper>
-      </Popper>
-    </>
-  )
+    return (
+        <>
+            <ButtonGroup variant="contained" ref={anchorRef}>
+                <Button sx={{ textTransform: "none", width: 100, fontSize: 16 }}>{props.selection}</Button>
+                <Button size="small" onClick={e => openList(e)}>{popen ? <ArrowDropUp /> : <ArrowDropDown />}</Button>
+            </ButtonGroup>
+            <Popper open={popen} anchorEl={anchorRef.current}>
+                <Paper sx={{ width: 145 }}>
+                    <MenuList>
+                        {options}
+                    </MenuList>
+                </Paper>
+            </Popper>
+        </>
+    )
 }
