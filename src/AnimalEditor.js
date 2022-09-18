b6b2038ef5f17158f7ba90e5137214cda6840026
Andrew Freeman <awfreeman1@gmail.com>
2022-09-18 13:25:48 -0400

    crap and things

--- src/AnimalEditor.js
+++ src/AnimalEditor.js
@@ -1,13 +1,18 @@
 import { Paper, Box, TextField, Stack, ImageList, ImageListItem, Button } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import { useRecoilState } from 'recoil'
-
+import { Image } from '@mui/icons-material'
+function ImageViewer(props) {
+    return (
+        <div class="imageViewerRoot">
+            {props.images.map(uri => <img src={uri} loading="lazy" />)}
+        </div>
+    )
+}
 export default function AnimalEditor(props) {
     const [editor, setEditor] = useRecoilState(props.state)
-    function dismiss(e) {
-        if (e == "exit" || e.target.id == "exit") {
-            setEditor(null)
-        }
+    function dismiss() {
+        setEditor(null)
     }
     if (editor == null)
         return <></>
@@ -23,34 +28,28 @@ export default function AnimalEditor(props) {
                     height: "100%",
                     bgcolor: "rgba(0, 0, 0, 0.5)"
                 }}
-                    onClick={e => dismiss(e)}
+                    onClick={e => dismiss()}
                     id="exit">
                 </Box>
                 <Box sx={{
-                    position: "absolute", 
-                    display: "flex", 
+                    position: "absolute",
+                    display: "flex",
                     alignItems: "center",
                     justifyContent: "center",
                     width: "100%",
                     height: "100%",
-                    top:0,
-                    right:0
+                    top: 0,
+                    right: 0
                 }}>
                     <Paper sx={{
                         minWidth: "70%",
-                        minHeight: "70%",
                         maxWidth: "95%",
                         maxHeight: "95%",
                         zIndex: 200,
+                        overflow: "scroll"
                     }}>
-                        <Button id="exit" onClick={() => dismiss("exit")} sx={{zIndex:200, bgcolor:"red", position:"absolute"}}><CloseIcon sx={{color:"white"}}/></Button>
-                        <ImageList sx={{ maxHeight:300}}>
-                            {editor.images.map(uri=> 
-                            <ImageListItem key={uri}>
-                                <img src={uri} loading="lazy"/>
-                            </ImageListItem>
-                            )}
-                        </ImageList>
+                        <Button variant="contained" id="exit" color="error" onClick={() => dismiss()} sx={{ zIndex: 200, color: "red", bgcolor: "red", position: "absolute", }}><CloseIcon sx={{ color: "white" }} /></Button>
+                        <ImageViewer images={editor.images} />
                         <Stack
                             component="form"
                             onSubmit={e => e.preventDefault()}
@@ -63,20 +62,20 @@ export default function AnimalEditor(props) {
                                 variant="outlined"
                                 label="Name"
                                 defaultValue={editor.name}
-                                sx={{ padding: "2px", marginY:"5px" }} />
+                                sx={{ padding: "2px", marginY: "5px" }} />
                             <TextField
                                 id="outlined-basic"
                                 variant="outlined"
                                 label="Species"
                                 defaultValue={editor.species}
-                                sx={{ padding: "2px", marginY:"5px" }} />
+                                sx={{ padding: "2px", marginY: "5px" }} />
                             <TextField
                                 id="outlined-basic"
                                 variant="outlined"
                                 multiline
                                 label="Comments"
                                 defaultValue={editor.comments}
-                                sx={{ padding: "2px", marginY:"5px" }} />
+                                sx={{ padding: "2px", marginY: "5px" }} />
                         </Stack>
                     </Paper>
                 </Box>
